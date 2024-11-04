3b904ef3208f5048642774920a19bb4bd0714dc0
Kumao-Y-I <denngeki8117@live.jp>
2024-11-04 19:40:13 +0900

    JavascriptでのTODOアプリ制作練習完了

--- main.js
+++ main.js
@@ -1,24 +1,73 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const onClickAdd = () =>{
+  // テキストボックスの値を取得し、その後初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value="";
 
-setupCounter(document.querySelector('#counter'))
+  // 未完了リストに追加
+  createIncompleteTodo(inputText);
+}
+
+// 渡された引数を基に未完了のTODOを作成する関数
+const createIncompleteTodo=(todo) =>{
+  // liタグ生成
+  const li = document.createElement("li");
+
+  // divタグ生成
+  const div = document.createElement("div");
+  div.className="list-row";
+
+  // pタグ生成
+  const p = document.createElement("p");
+  p.className="todo-item";
+  p.innerText=todo;
+
+  // ボタン完了タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText= "完了";
+  completeButton.addEventListener("click",() =>{
+
+    // 押された完了ボタンの親にあるliタグ配下の完了ボタンと削除ボタンを削除
+    const moveTarget = completeButton.closest("li");
+    completeButton.nextElementSibling.remove();
+    completeButton.remove();
+
+    // 戻すボタンを生成してdivタグ配下に生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click",() =>{
+
+      //TODOの内容を取得し、未完了リストに追加
+      const todoText = backButton.previousElementSibling.innerText;
+      createIncompleteTodo(todoText);
+      backButton.closest("li").remove();
+    })
+    moveTarget.firstElementChild.appendChild(backButton);
+
+    // 完了リストに移動
+    document.getElementById("complete-list").appendChild(moveTarget);
+
+  });
+
+  // ボタン削除タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText= "削除";
+  deleteButton.addEventListener("click",() =>{
+      // 押された削除ボタンの親にあるliタグを未完了リストから削除
+      const deleteTarget = deleteButton.closest("li");
+      document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
+  // liタグの子要素に各要素を設定
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+  li.appendChild(div);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+}
+
+document.getElementById("add-button").addEventListener("click",onClickAdd);
+
